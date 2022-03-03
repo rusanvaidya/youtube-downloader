3d33d4a97bac7f36a1c593b811b62b114bcbd84e
Rusan Vaidya <60218372+rusanvaidya@users.noreply.github.com>
2022-03-03 12:20:26 +0545

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.100']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
